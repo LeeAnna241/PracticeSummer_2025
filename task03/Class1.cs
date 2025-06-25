040861172469355a08108844e5c2ce6138a0c124
LeeAnna241 <anastasiakisner6@gmail.com>
2025-06-26 00:00:14 +0600

    task03 и task03tests

--- task03/Class1.cs
+++ task03/Class1.cs
@@ -1,6 +1,36 @@
-﻿namespace task03;
+﻿using System.Collections;
+using System.Collections.Generic;
 
-public class Class1
+namespace task03
 {
+    public class CustomCollection<T> : IEnumerable<T>
+    {
+        private readonly List<T> _items = new();
 
+        public void Add(T item) => _items.Add(item);
+        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerable<T> GetReverseEnumerator()
+        {
+            int amount = _items.Count();
+            for (var i = amount - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        public static IEnumerable<int> GenerateSequence(int start, int count)
+        {
+            for (var i = start; i < count + start; i++)
+            {
+                yield return i;
+            }
+        }
+
+        public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+        => _items
+        .Where(predicate)
+        .OrderBy(x => x);
+    }
 }
--- task03tests/UnitTest1.cs
+++ task03tests/UnitTest1.cs
@@ -1,10 +1,56 @@
-﻿namespace task03tests;
+﻿using Xunit;
+using task03;
 
-public class UnitTest1
+public class IteratorTests
 {
+    //стандартный итератор
     [Fact]
-    public void Test1()
+    public void CustomCollection_GetEnumerator_ReturnsAllItems()
     {
+        var collection = new CustomCollection<int>();
+        collection.Add(1);
+        collection.Add(2);
 
+        var result = new List<int>();
+        foreach (var item in collection)
+        {
+            result.Add(item);
+        }
+
+        Assert.Equal(new[] { 1, 2 }, result);
+    }
+
+    [Fact]
+    public void GetReverseEnumerator_ReturnsItemsInReverseOrder()
+    {
+        var collection = new CustomCollection<int>();
+        collection.Add(1);
+        collection.Add(2);
+
+        var result = collection.GetReverseEnumerator().ToList();
+        Assert.Equal(new[] { 2, 1 }, result);
+    }
+
+    //генерирует последовательность чисел
+    [Fact]
+    public void GenerateSequence_ReturnsCorrectSequence()
+    {
+        var sequence = CustomCollection<int>.GenerateSequence(5, 3);
+        sequence.ToList();
+        Assert.Equal(new[] { 5, 6, 7 }, sequence);
+    }
+
+    //фильтрует коллекцию
+    [Fact]
+    public void FilterAndSort_ReturnsFilteredAndSortedItems()
+    {
+        var collection = new CustomCollection<int>();
+        collection.Add(3);
+        collection.Add(1);
+        collection.Add(2);
+
+        var result = collection.FilterAndSort(x => x > 1, x => x);
+        result.ToList();
+        Assert.Equal(new[] { 2, 3 }, result);
     }
 }
