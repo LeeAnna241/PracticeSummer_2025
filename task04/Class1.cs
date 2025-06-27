1109a19ac914108d141a671bfcc563bba29d4e17
LeeAnna241 <anastasiakisner6@gmail.com>
2025-06-27 19:17:50 +0600

    task 04 и task04tests

---
+++ task04/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace task04
+{
+    public interface ISpaceship
+    {
+        void MoveForward();      // Движение вперед
+        void Rotate(int angle);  // Поворот на угол (градусы)
+        void Fire();             // Выстрел ракетой
+        int Speed { get; }       // Скорость корабля
+        int FirePower { get; }   // Мощность выстрела
+    }
+    public class Fighter : ISpaceship
+    {
+        public void MoveForward() {}
+        public void Rotate(int angle) {}
+        public void Fire() {}
+        public int Speed
+        {
+            get { return 100; }
+        }
+        public int FirePower
+        {
+            get { return 50; }
+        }
+    }
+    public class Cruiser : ISpaceship
+    {
+        public void MoveForward() {}
+        public void Rotate(int angle) {}
+        public void Fire() {}
+        public int Speed
+        {
+            get { return 50; }
+        }
+        public int FirePower
+        {
+            get { return 100; }
+        }
+    }
+}
