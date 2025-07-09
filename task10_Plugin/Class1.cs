2e923375f3e2341f7c72fbf87b4b0fc2fc20043b
LeeAnna241 <anastasiakisner6@gmail.com>
2025-07-09 11:49:31 +0600

    Plugin и PluginLoader

--- task10_Plugin/Class1.cs
+++ task10_Plugin/Class1.cs
@@ -1,6 +1,17 @@
-﻿namespace task10_Plugin;
+﻿namespace Plugin;
 
-public class Class1
+public interface IPlugin
 {
-
+    void Execute();
 }
+
+public class PluginLoad : Attribute
+{
+    public string[] Dependence { get; }
+
+        public PluginLoad(params string[] depends_on)
+        {
+            Dependence = depends_on;
+        }
+}
+
