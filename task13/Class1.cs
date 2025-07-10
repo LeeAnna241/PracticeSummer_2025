c52e2976d5c3f9d183f9e416569504bf135f8bbf
LeeAnna241 <anastasiakisner6@gmail.com>
2025-07-10 19:36:30 +0600

    task13 и task13tests

--- task13/Class1.cs
+++ task13/Class1.cs
@@ -1,6 +1,19 @@
-﻿namespace task13;
+﻿using System.Text.Json.Serialization;
 
-public class Class1
+namespace task13;
+
+public class Subject
 {
-
+  public string? Name {get; set; }
+  public int Grade {get; set; }
 }
+
+public class Student
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    public List<Subject>? Grades { get; set; }
+}
+
