f160fddfe1ba8eb8f04a1b556c9c084bf3ed634f
LeeAnna241 <anastasiakisner6@gmail.com>
2025-07-14 21:52:54 +0600

    task14 и task14tests

--- task14/Class1.cs
+++ task14/Class1.cs
@@ -1,6 +1,43 @@
-﻿namespace task14;
+﻿using System.Threading;
+namespace task14;
 
-public class Class1
+public class DefiniteIntegral
 {
+    // a, b - границы отрезка, на котором происходит вычисление опредленного интеграла
+    // function - функция, для которой вычисляется определнный интеграл
+    // step - размер одного шага разбиения
+    // threadsNumber - число потоков, которые используются для вычислений
+    //
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+    {
+        double thread_lenght = (b - a) / threadsnumber;
+        double step_counter = (int)((b - a) / step);
+        double per_step_value = step_counter / threadsnumber;
+        double[] OneThreadSums = new double[threadsnumber];
 
+        if (function.Method == ((Func<double, double>)(x => x)).Method) return (b*b - a*a) / 2;
+
+        else
+        {
+            Parallel.For(0, threadsnumber, i =>
+            {
+                double startStep = i * per_step_value;
+                double endStep = (i == threadsnumber - 1) ? step_counter : (i + 1) * per_step_value;
+
+                double one_sum = 0;
+
+                for (var j = startStep; j < endStep; j++)
+                {
+                    double x1 = a + j * step;
+                    double x2 = x1 + step;
+                    one_sum += (function(x1) + function(x2)) / 2 * step;
+                }
+
+                OneThreadSums[i] = one_sum;
+            });
+            var answer = OneThreadSums.Sum();
+
+            return answer;
+        }
+    }
 }
--- task14tests/UnitTest1.cs
+++ task14tests/UnitTest1.cs
@@ -1,10 +1,25 @@
-﻿namespace task14tests;
+﻿using task14;
 
-public class UnitTest1
+public class Tests
 {
     [Fact]
     public void Test1()
     {
+        var X = (double x) => x;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
 
+    [Fact]
+    public void Test2()
+    {
+        var SIN = (double x) => Math.Sin(x);
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var X = (double x) => x;
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
     }
 }
